56eab7b474ecebc4cb1eeeb243639c36b40bedcc
akarshrajap <akarshrajap@gmail.com>
2026-02-26 15:46:51 +0530

    Initial commit: Frontend and Backend

--- Frontend/src/Api/axios.js
+++ Frontend/src/Api/axios.js
@@ -5,6 +5,9 @@ const API = axios.create({
     baseURL: 'http://localhost:4000/api', // or your Render URL
 });
 
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:4000';
+
+
 // Add the interceptor to THIS instance
 API.interceptors.request.use((config) => {
     const token = localStorage.getItem('token');
